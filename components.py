bbe0d51057a5996178fc9066c7a19c9c7f00bd83
れん <ren.komatsu.5@gmail.com>
2025-12-03 07:22:53 +0900

    モード選択をサイドバーに変更、マジックナンバーを定数化、検索ドキュメント数を3から5に変更

--- components.py
+++ components.py
@@ -23,16 +23,15 @@ def display_app_title():
 
 def display_select_mode():
     """
-    回答モードのラジオボタンを表示
+    回答モードのラジオボタンをサイドバーに表示
     """
-    # 回答モードを選択する用のラジオボタンを表示
-    col1, col2 = st.columns([100, 1])
-    with col1:
-        # 「label_visibility="collapsed"」とすることで、ラジオボタンを非表示にする
+    # サイドバーに回答モードを選択する用のラジオボタンを表示
+    with st.sidebar:
+        st.markdown("### モード選択")
         st.session_state.mode = st.radio(
-            label="",
+            label="利用目的を選択してください",
             options=[ct.ANSWER_MODE_1, ct.ANSWER_MODE_2],
-            label_visibility="collapsed"
+            label_visibility="visible"
         )
 
 
